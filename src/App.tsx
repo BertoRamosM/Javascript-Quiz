89ed138f016e0821d2bceca028f8045d7dd2b7ca
ARamos <albertramos902@gmail.com>
2024-04-14 14:50:34 +0100

    data with questions/answers json created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import JavascriptLogo from "./assets/JavascriptLogo";
+import { Container, Stack, Typography } from "@mui/material";
 
 function App() {
   return (
-    <>
-      <h1>Javascript Quiz</h1>
-    </>
+    <main>
+      <Container maxWidth="sm">
+        <Stack direction="row" alignItems="center" justify="center">
+          <Typography variant="h2" component="h1">
+            <JavascriptLogo />
+            Javascript Quiz
+          </Typography>
+        </Stack>
+      </Container>
+    </main>
   );
 }
 
