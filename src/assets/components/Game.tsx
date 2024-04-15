df15f4db1c04c402ebd70705f17576ed2dada7d1
ARamos <albertramos902@gmail.com>
2024-04-15 11:17:55 +0100

    working on user selected button

--- src/assets/components/Game.tsx
+++ src/assets/components/Game.tsx
@@ -14,9 +14,40 @@ import { type Question as QuestionType } from "../../types";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { hybrid } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
+
+  const getBackgroundColor = (info: QuestionType, index: number) => {
+    const { userSelectedAnswer, correctAnswer } = info
+
+    //if user hasnt press any button
+    if (userSelectedAnswer === null) return 'transparent'
+    
+    //if user has chosen but answer its incorrect
+    if (index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+
+    //if user has chosen the correct answer
+    if (index === correctAnswer) return 'green'
+
+    //if user has chosen the wrong answer
+    if (index === userSelectedAnswer) return 'red'
+    
+    //if any of the rest
+    return "transparent";
+  };
+
+
 const Question = ({ info }: { info: QuestionType }) => {
+  const selectAnswers = useQuestionsStore(state => state.selectAnswer)
+
+  const handleClick = (answerIndex: number) => () => {
+    selectAnswers(info.id, answerIndex)
+  }
+
+
   return (
-    <Card variant="outlined" sx={{ bgcolor: "#222", textAlign: "left", p: 2, marginTop: 4}}>
+    <Card
+      variant="outlined"
+      sx={{ bgcolor: "#222", textAlign: "left", p: 2, marginTop: 4 }}
+    >
       <Typography variant="h5" color="lightBlue">
         {info.question}
       </Typography>
@@ -27,9 +58,14 @@ const Question = ({ info }: { info: QuestionType }) => {
 
       <List sx={{ bgcolor: "#333" }} disablePadding>
         {info.answers.map((answer, index) => (
-          <ListItem key={index}>
-            <ListItemButton sx={{ textAlign: 'center' }} divider>
-              <ListItemText primary={answer} />
+          <ListItem key={index} divider>
+            <ListItemButton
+              sx={{
+                backgroundColor: getBackgroundColor(info, index)
+              }}
+              disabled={info.userSelectedAnswer !== null}
+              onClick={handleClick(index) }>
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
             </ListItemButton>
           </ListItem>
         ))}
