ae679af1960d7182564b599de9489a9927a53a1f
ARamos <albertramos902@gmail.com>
2024-04-15 10:42:55 +0100

    logic for choosing the answer of the user created

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -6,6 +6,7 @@ interface State{
   currentQuestion: number,
   loading: boolean,
   fetchQuestions: (limit:number)=> Promise<void>
+  selectAnswer: (questionId: number, answerIndex: number) => void
 }
 
 export const useQuestionsStore = create<State>((set, get) => {
@@ -28,6 +29,28 @@ export const useQuestionsStore = create<State>((set, get) => {
       } finally {
         set({loading: false})
       }
+    },
+    selectAnswer: (questionId: number, answerIndex: number) => {
+      // we get the state
+      const {questions} = get()
+      //we use the structured clone, that its used to clone the object
+      const newQuestions = structuredClone(questions)
+      //we find the index of the question by using the parameter questionId
+      const questionIndex = newQuestions.findIndex(q => q.id === questionId)
+      //by using the index of the question, we get the info from the copied object
+      const questionInfo = newQuestions[questionIndex]
+      //if the answeIndex that we pass in parameter its the .correct answer, the response its correct
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+      
+      //change this information in the copy of the question
+      newQuestions[questionIndex] = {
+        ...questionInfo,
+        isCorrectUserAnswer,
+        userSelectedAnswer: answerIndex
+      }
+
+      //upload the state 
+      set({questions: newQuestions})
     }
   }
 })
