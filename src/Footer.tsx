dadb5fa9503e02c38a27fc00350f1019a9021bae
ARamos <albertramos902@gmail.com>
2024-04-16 17:33:23 +0100

    button to restart the game

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,11 +1,22 @@
 import { useQuestionData } from "./hooks/useQuestionData";
+import { Button } from "@mui/material";
+import { useQuestionsStore } from "./store/questions";
+
 const Footer = () => {
 
-  const {correct, incorrect, unanswered} = useQuestionData()
+  const { correct, incorrect, unanswered } = useQuestionData()
+  
+  const reset = useQuestionsStore(state => state.reset)
 
   return (
     <footer style={{ marginTop: "16px" }}>
       <strong>{`${correct} Correct - ${incorrect} Incorrect - ${unanswered} Unanswered`}</strong>
+
+      <div style={{marginTop: '16px'}}>
+        <Button onClick={() => reset()}>
+          Reset Game
+        </Button>
+      </div>
     </footer>
   );
 }
