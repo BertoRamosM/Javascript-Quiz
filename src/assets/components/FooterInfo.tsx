5da174723a10937be587a369eef0944a5321b46c
ARamos <albertramos902@gmail.com>
2024-04-28 15:26:57 +0100

    added githubicon

--- src/assets/components/FooterInfo.tsx
+++ src/assets/components/FooterInfo.tsx
@@ -1,10 +1,21 @@
+import GithubIcon from "../GithubIcon";
 
 const FooterInfo = () => {
     const githubUrl = "https://github.com/BertoRamosM"
 
   return (
     <div style={{ paddingTop: "1rem" }}>
-      <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+      <a
+        href={githubUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <GithubIcon />
         <p style={{ fontSize: "0.8rem" }}> Github | BertoRamosM</p>
       </a>
     </div>
